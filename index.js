b4752369cda7a52a4006c951aa7c240ba8abb3c6
David Yan <daveddd97@hotmail.com>
2019-11-14 10:34:31 +0100

    user can now delete book

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 const path = require("path");
 
+app.use(bodyParser.json());
+
 app.use(express.static('public'))
+
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
 
-let nrOfItems = 0
+let books = [
+    {
+        book: 'Den slitna',
+        id: 1,
+    },
+    {
+        book: 'Den glada',
+        id: 2,
+    },
+]
 
-app.get('/api', function (err, res, req) {
-    res.send({ counter: nrOfItems++})
+
+app.get('/books', function (req, res) {
+    res.send({ books })
 })
+
+app.post('/add/book', function (req, res) {
+    var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    var uniqid = randLetter + Date.now();
+
+    books.push(
+        {
+            book: req.body.bookName,
+            id: uniqid,
+        }
+    )
+    res.send({ books })
+})
+
+app.delete('/delete/book', function (req, res) {
+    for (var i = books.length - 1; i >= 0; i--) {
+        if (req.body.bookName == books[i].book) {
+            books.splice(i, 1)
+        }
+    }
+
+    res.send({ books })
+})
+
 app.listen(port, () => console.log('Exempel app listening on port ${port}!'));
--- public/static/script/script.js
+++ public/static/script/script.js
@@ -1,15 +1,86 @@
 // Make a request for a user with a given ID
-function increase() {
-  // code
-  axios.get('http://localhost:3000/api')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-    document.getElementById("counter").innerHTML = response.data.counter
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+function getBooks() {
+    // code
+    axios.get('http://localhost:3000/books')
+    .then(function (response) {
+      // handle success
+      console.log(response)
+      renderBooks(response.data.books)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+}
+
+function renderBooks(books) {
+    var bookElement = document.getElementById("books")
+    bookElement.innerHTML = ""
+
+    for (var i = 0; i < books.length; i++) {
+        bookElement.innerHTML += "<input type='checkbox' name='check' value='" + books[i].book + "'>" + books[i].book  + "<br>"
+    }
+}
+
+
+function addBook(event) {
+    event.preventDefault();
+    // code
+
+    var bookName = document.getElementById("book").value
+    
+    axios.post('http://localhost:3000/add/book', {
+      bookName: bookName
+    }, 
+    {
+      headers: {
+        'Content-Type': 'application/json',
+    }
+    })
+    .then(function (response) {
+      // handle success
+      if(response.status = 200) {
+        getBooks()
+      }
+    
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+}
+
+function selectedBookValue() {
+    var selectedBook = document.getElementsByName('check');     
+    
+    for (i = 0; i < selectedBook.length; i++) { 
+        if (selectedBook[i].checked) { 
+            var bookName = selectedBook[i].value
+            document.getElementById("result").innerHTML = "Du har valt: " + selectedBook[i].value; 
+        } 
+    }
+
+    deleteBook(bookName)
+}
+
+function deleteBook(bookName) {
+    // code
+    axios.delete('http://localhost:3000/delete/book', {
+      headers: {
+        'Content-Type': 'application/json',
+      }, 
+      data: {
+          bookName: bookName
+      },
+    })
+    .then(function (response) {
+      if(response.status = 200) {
+        getBooks()
+      }
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
 }
 
